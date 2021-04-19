1c27a0dda0a79f7aaf55ad29621dc1214d1e8b28
Mattias Martens <mattiasofmartens@gmail.com>
2021-04-18 18:07:49 -0700

    remove JS files in source

--- src/patterns/sets.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.createSetFromNullable = void 0;
-exports.createSetFromNullable = function (t) { return t === undefined ? new Set() : new Set([t]); };
