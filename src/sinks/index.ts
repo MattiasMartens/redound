bd50bb3b80048ee1775bbce8f28fd52cd79bd9a5
Mattias Martens <mattiasofmartens@gmail.com>
2021-04-27 22:10:24 -0700

    exports

--- src/sinks/index.ts
+++ src/sinks/index.ts
@@ -1,3 +1,4 @@
 export * from './console-logger'
 export * from './for-each'
 export * from './collector'
+export * from './node-writable'
--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,2 +1,4 @@
 export * from './iterable'
 export * from './manual'
+export * from './node-readable'
+export * from './queryable'
